4c7f3d26eae6b7c44875575b230c9bdda994fbe8
ShinDodam <tlseheka@naver.com>
2022-08-06 12:00:01 +0900

    change

--- recommendapp/urls.py
+++ recommendapp/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from recommendapp.views import Suggest
+from recommendapp.views import ReDetailView, ReListView
 
 app_name = "recommendappp"
 
 urlpatterns = [
-    path('Suggest/', Suggest, name='Suggest')
+    path('list/', ReListView.as_view(), name='list'),
+    path('detail/<int:pk>', ReDetailView.as_view(), name='detail'),
 ]
--- recommendapp/views.py
+++ recommendapp/views.py
@@ -1,5 +1,13 @@
 # Create your views here.
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.views.generic import DetailView, ListView
 
-def Suggest(request):
-    return render(request, 'recommendapp/suggest.html')
+class ReListView(ListView):
+    model = User
+    template_name = 'recommendapp/list.html'
+    paginate_by = 10
+
+
+class ReDetailView(DetailView):
+    model = User
+    template_name = 'recommendapp/detail.html'
