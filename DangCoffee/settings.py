882c4f5380c97b64d7c29be1d239b4dba1d23a92
jangseoyoung98 <jangseoyoung98@gmail.com>
2022-08-10 00:50:07 +0900

    make mypage

--- DangCoffee/settings.py
+++ DangCoffee/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'recommend',
     'accounts',
+    'mypage',
 
     'django.contrib.sites',
     'allauth',
--- DangCoffee/urls.py
+++ DangCoffee/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.urls import path, include
 from recommend import views
 from accounts import views as accounts_views
+from mypage import views as mypage_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -16,5 +17,7 @@ urlpatterns = [
     path('logout/', accounts_views.logout, name='logout'),
     path('signup/', accounts_views.signup, name='signup'),
     path('accounts/', include('allauth.urls')),
+
+    path('mypage/', mypage_views.first),
 
 ]
