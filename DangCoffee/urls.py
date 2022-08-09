8bec53e11c6875d362e67e2e062837bc37df2462
jangseoyoung98 <jangseoyoung98@gmail.com>
2022-08-10 00:05:39 +0900

    ㅁaccounts - 로그인, ë로그인 ís - 1차 완료(로그인 페이지, 구글 계정 연동)

--- DangCoffee/urls.py
+++ DangCoffee/urls.py
@@ -2,12 +2,19 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from recommend import views # views 이름 겹치는 것 수정!!
+from recommend import views
+from accounts import views as accounts_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
+
     path('recommend1/', views.recommend1),
     path('recommend2/', include('recommend.urls')),
-    path('accounts/', include('accounts.urls'))
+
+    path('login/', accounts_views.login, name="login"),
+    path('logout/', accounts_views.logout, name='logout'),
+    path('signup/', accounts_views.signup, name='signup'),
+    path('accounts/', include('allauth.urls')),
+
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,21 +3,36 @@ from django.contrib import auth
 from django.contrib.auth.models import User
 
 def login(request):
-    # POST 요청이 들어오면 로그인 처리
     if request.method == 'POST':
         userid = request.POST['username']
         pwd = request.POST['password']
         user = auth.authenticate(request, username=userid, password=pwd)
+
         if user is not None:
             auth.login(request, user)
             return redirect('home')
         else:
-            return render(request, 'login.html')
+            return render(request, 'failed_login.html')
 
-    # GET 요청이 들어오면 login form을 담고 있는 login.html로 이동
     else:
         return render(request, 'login.html')
 
 def logout(request):
     auth.logout(request)
     return redirect('home')
+
+def signup(request):
+    if request.method == "POST":
+        if request.POST['password'] == request.POST['repeat']:
+            # 회원가입
+            new_user = User.objects.create_user(username=request.POST['username'], password=request.POST['password'])
+            # 로그인
+            auth.login(request, new_user)
+            # 홈 리다이렉션
+            return redirect('home')
+    return render(request, 'register.html')
+
+
+
+
+
