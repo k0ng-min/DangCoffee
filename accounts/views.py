35c311093ad0db93a754e2f2c30ba017e71eb0e2
jangseoyoung98 <jangseoyoung98@gmail.com>
2022-08-15 23:59:00 +0900

    fix accounts (not success)

--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,7 @@ from django.contrib.auth.hashers import make_password
 from django.shortcuts import render, redirect
 from django.contrib import auth
 from django.contrib.auth.models import User # User라고 하는 내장된 객체(테이블)을 장고는 이미 갖고 있음
+from .models import User as UserOrigin
 
 def login(request):
     # POST 요청이 들어오면, 로그인 처리
@@ -29,9 +30,9 @@ def signup(request):
         return render(request, 'signup.html')
 
     elif request.method == "POST":
-        username = request.POST.get['User ID', None]    # 딕셔너리 형태
-        password = request.POST.get['Password', None]
-        re_password = request.POST.get['Password (Repeat)', None]
+        username = request.POST.get('User ID', None)
+        password = request.POST.get('Password', None)
+        re_password = request.POST.get('Password (Repeat)', None)
         res_data = {}
         if not (username and password and re_password):
             res_data['error'] = "모든 값을 입력해야 합니다."
@@ -40,7 +41,8 @@ def signup(request):
         else:
             user = User(username=username, password=make_password(password))
             user.save()
-        return render(request, 'success_signup.html')
+            return render(request, 'success_signup.html')
+        return render(request, 'signup.html')
 
 
 
